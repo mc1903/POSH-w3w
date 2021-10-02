bde5d86e9d71ec1deba0b222195fd2eb1e6a4c59
mc1903 <mc1903@users.noreply.github.com>
2021-10-02 23:37:20 +0100

    Added Convert-3waToCoordinates
    Added Convert-3waToCoordinates.ps1
    Updated Posh-w3w.psd1 to export the Convert-3waToCoordinates function

--- Posh-w3w.psd1
+++ Posh-w3w.psd1
@@ -66,10 +66,10 @@
     # NestedModules = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport = @('Convert-CoordinatesTo3wa')
+    FunctionsToExport = @('Convert-CoordinatesTo3wa', 'Convert-3waToCoordinates')
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-    CmdletsToExport = @()
+    # CmdletsToExport = @()
 
     # Variables to export from this module
     # VariablesToExport = @()
---
+++ Src/Public/Convert-3waToCoordinates.ps1
@@ -0,0 +1,74 @@
+<#
+	.Notes
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Function Name:		Convert-3waToCoordinates
+	Module Name:		Posh-w3w
+	Created by:			Martin Cooper (@mc1903)
+	Date:				02/10/2021
+	GitHub:				https://github.com/mc1903/POSH-w3w
+	Version:			1.0.0
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	.Synopsis
+	This function converts a w3w address to a latitude and longitude.
+
+	.Description
+	This function converts a w3w address to a latitude and longitude 
+
+	.Parameter APIKey
+	The w3w API key from your account - https://developer.what3words.com/public-api
+
+	.Parameter Words
+	The w3w address. It must be three words separated with dots.
+
+	.Parameter Format
+	The preferred output format (json or geojson).
+
+	.Example
+	# Get the w3w address for Gandalf Corner, London
+	Convert-3waToCoordinates -APIKey SP****TM -Words forget.glory.mount
+#>
+Function Convert-3waToCoordinates {
+    [CmdletBinding(
+        SupportsShouldProcess=$False
+    )]
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [String[]] $APIKey,
+
+        [Parameter(Mandatory=$True)]
+        [ValidatePattern("^\p{L}+\.\p{L}+\.\p{L}+$")]
+        [Alias("3wa")]
+        [String[]] $Words,
+
+        [Parameter(Mandatory=$False)]
+        [ValidateSet("json","geojson")]
+        [String[]] $Format
+    )
+
+    If(!$Format){
+        $Format="json"
+    }
+
+    #Force TLS 1.2 Connections
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    #Create Header Parameters
+    $Header=@{}
+    $Header.add("X-Api-Key","$APIKey")
+
+    Try {
+      Invoke-RestMethod -Method GET -Uri "https://api.what3words.com/v3/convert-to-coordinates?words=$($Words)&format=$($Format)" -Headers $Header -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -OutVariable Output -ErrorVariable OutError | Out-Null
+    } 
+    Catch {
+        $OutError = $_
+    }
+   
+    If ($OutError) {
+        Write-Error $OutError
+    } Else {
+        $Output | Format-List
+    }
+
+}
